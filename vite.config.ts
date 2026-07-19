c57cbae66db76080a2aea0f30dadc23e0edba576
B tilak kumar <boyatilak143@gmail.com>
2026-07-19 15:57:01 +0530

    fix: load native environment variable configurations in vite build context

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file from process.env so Netlify variables are included
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  }
+})
